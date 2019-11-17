0ae28ee0e1312ec7315b25f41c8b42c8777ab1c7
Christian Oder <myself5@carbonrom.org>
2019-11-17 18:13:00 +0100

    soong: Whitelist cpio for prebuilt ramdisk extraction
    Change-Id: I46321d11823a623635e9776c6c7c1c92b3eadccb

--- ui/build/paths/config.go
+++ ui/build/paths/config.go
@@ -78,6 +78,7 @@ var Configuration = map[string]PathConfig{
         "cpio":    Allowed,
 	"brotli":  Allowed,
 	"ccache":  Allowed,
+	"cpio":    Allowed,
 	"dd":      Allowed,
 	"diff":    Allowed,
 	"dlv":     Allowed,
